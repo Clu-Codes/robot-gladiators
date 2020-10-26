47183bf6167174a48e5abb140f43bd9f36cb3e30
Clu <retsulcconnor@gmail.com>
2020-10-25 21:49:21 -0400

    Added console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! Javascript is runnign!");
+var playerName = window.prompt("What is your player's name?");
+console.log(playerName);
+console.log("This logs a string");
+console.log(10 + 10);
+console.log("Our robot's name is " +playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
