845775239025cba6efe873707b074ae11804406c
Clu <retsulcconnor@gmail.com>
2020-10-27 18:56:14 -0400

    Added startGame to restore player health and allow for continuous play

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,16 +65,27 @@ var fight = function(enemyName) {
         }  
     }
 }
+    var startGame = function() {
+        // reset player stats
+        playerHealth = 100;
+        playerAttack = 10;
+        playerMoney = 10;
 
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
-    }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
+        // debugger;
+        for(var i = 0; i < enemyNames.length; i++) {
+            if (playerHealth > 0) {
+                window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+    
+                var pickedEnemyName = enemyNames[i];
+                enemyHealth = 50;
+                fight(pickedEnemyName);
+            }
+            else {
+                window.alert("You have lost your robot in battle! Game Over!");
+                break;
+            }   
+        }
+        startGame();
+    };
+// start the game when the page loads
+startGame();
